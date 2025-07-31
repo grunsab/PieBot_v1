c41e64905aac843955a3fd352e5cf8fee6cb4d1a
Rishi S <rishi@talkdoc.com>
2025-07-30 17:52:33 -0700

    Update UCI_engine to use MCTS_profiling_speedups code

--- uci_engine.py
+++ uci_engine.py
@@ -19,7 +19,7 @@ import time
 import threading
 from queue import Queue
 import AlphaZeroNetwork
-import MCTS
+import MCTS_profiling_speedups as MCTS
 from device_utils import get_optimal_device, optimize_for_device
 from quantization_utils import load_quantized_model
 
@@ -596,6 +596,8 @@ class UCIEngine:
                 elif command == "ucinewgame":
                     # Reset board for new game
                     self.board = chess.Board()
+                    if self.mcts_engine:
+                        self.mcts_engine.clear_caches()
                 elif self.verbose:
                     print(f"info string Unknown command: {command}")
                     sys.stdout.flush()
